83fc9c1eb52f34237909e43e972f911a3943eb3c
nikonovilya <inikonovilya@gmail.com>
2025-07-29 23:52:47 +0500

    [~] Update table resize

--- src/scripts/core/dom.js
+++ src/scripts/core/dom.js
@@ -1,6 +1,9 @@
 class Dom {
   constructor(selector) {
-    this.$el = typeof selector === 'string' ? document.querySelector(selector) : selector;
+    this.$el =
+      typeof selector === 'string'
+        ? document.querySelector(selector)
+        : selector;
   }
 
   html(html = '') {
@@ -34,6 +37,28 @@ class Dom {
       this.$el.appendChild(node);
     }
     return this;
+  }
+
+  closest(selector) {
+    return $(this.$el.closest(selector));
+  }
+
+  getCoords() {
+    return this.$el.getBoundingClientRect();
+  }
+
+  get data() {
+    return this.$el.dataset;
+  }
+
+  findAll(selector) {
+    return this.$el.querySelectorAll(selector);
+  }
+
+  css(styles = {}) {
+    Object.keys(styles).forEach((key) => {
+      this.$el.style[key] = styles[key];
+    });
   }
 }
 
